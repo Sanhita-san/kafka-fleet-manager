382db479c8f638df7d9b859cd1989e480a2abf29
Sanhita-san <gssanhita@gmail.com>
2025-08-10 10:09:11 +0530

    Updated nav and font

---
+++ src/components/navbar/navLinks.js
@@ -0,0 +1,25 @@
+// Nav
+import { AiOutlineHome } from "react-icons/ai";
+import { SiKubernetes } from "react-icons/si";
+import { SiApachekafka } from "react-icons/si";
+import { MdOutlineSettings } from "react-icons/md";
+import { MdOutlineTroubleshoot } from "react-icons/md";
+import { MdRocketLaunch } from "react-icons/md";
+import { MdOutlineHub } from "react-icons/md";
+
+// Bottom Nav
+import { MdVpnKey } from "react-icons/md";
+import { MdCreditCard } from "react-icons/md";
+import { MdSupportAgent } from "react-icons/md";
+
+export const navLinks = [
+  AiOutlineHome,
+  SiKubernetes,
+  SiApachekafka,
+  MdOutlineSettings,
+  MdOutlineTroubleshoot,
+  MdRocketLaunch,
+  MdOutlineHub,
+];
+
+export const bottomNavLinks = [MdVpnKey, MdCreditCard, MdSupportAgent];
