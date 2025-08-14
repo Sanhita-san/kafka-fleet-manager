640bb9ba15e28e97e6e7f835741ef07baa724a7c
Sanhita-san <gssanhita@gmail.com>
2025-08-14 10:26:04 +0530

    Updated Billing Table

--- src/components/billing/BillingTable.jsx
+++ src/components/billing/BillingTable.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
 import styles from "./BillingTable.module.css";
 import { billingData } from "./billingData";
@@ -8,8 +8,11 @@ import dayjs from "dayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { Chip, Stack } from "@mui/material";
 
+// Icon
 import { MdKeyboardArrowDown } from "react-icons/md";
+import { BsSortAlphaDown, BsSortAlphaUp } from "react-icons/bs";
 
 // Color code
 const getUsageLevel = (value) =>
@@ -18,9 +21,56 @@ const getUsageLevel = (value) =>
 const getAmountLevel = (value) =>
   value === 0 ? styles.green : value < 3 ? styles.orange : styles.red;
 
+// Main Component
 export default function BillingTable() {
-  const [startDate, setStartDate] = useState(dayjs("2022-04-17"));
-  const [endDate, setEndDate] = useState(dayjs("2022-04-17"));
+  // Date pick logic
+  const [startDate, setStartDate] = useState(dayjs("2025-01-01"));
+  const [endDate, setEndDate] = useState(dayjs("2025-08-17"));
+  const handlePreset = (preset) => {
+    const today = new Date();
+    let start, end;
+
+    if (preset === "thisMonth") {
+      start = new Date(today.getFullYear(), today.getMonth(), 1);
+      end = today;
+    } else if (preset === "lastMonth") {
+      start = new Date(today.getFullYear(), today.getMonth() - 1, 1);
+      end = new Date(today.getFullYear(), today.getMonth(), 0);
+    } else if (preset === "last30") {
+      start = new Date(today);
+      start.setDate(start.getDate() - 30);
+      end = today;
+    }
+
+    setStartDate(dayjs(start.toISOString().slice(0, 10)));
+    setEndDate(dayjs(end.toISOString().slice(0, 10)));
+  };
+
+  // Accordion logic
+  const [open, setOpen] = useState(() => {
+    const initialState = {};
+    billingData.forEach((group) => {
+      initialState[group.id] = true; // open every group initially
+    });
+    return initialState;
+  });
+  const toggle = (id) => setOpen((prev) => ({ ...prev, [id]: !prev[id] }));
+
+  // Sorting Data logic
+  const [sortDir, setSortDir] = useState("asc"); // "asc", "desc"
+  const toggleSort = () => {
+    setSortDir((prev) => (prev === "asc" ? "desc" : "asc"));
+  };
+
+  // Sort logic
+  const sortedData = useMemo(() => {
+    if (!sortDir) return billingData;
+    return [...billingData].sort((a, b) =>
+      sortDir === "asc"
+        ? a.totalAmount - b.totalAmount
+        : b.totalAmount - a.totalAmount
+    );
+  }, [sortDir]);
 
   return (
     <div className={styles.billingData}>
@@ -28,62 +78,108 @@ export default function BillingTable() {
 
       {/* Date Picker */}
       <div className={styles.headerInfo}>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <p>Filter by Date</p>
-          <div className={styles.dateContainer}>
-            <DatePicker
-              label="Start"
-              slotProps={{
-                textField: { size: "small" },
-                actionBar: { actions: ["today"] },
-              }}
-              value={startDate}
-              onChange={(newStartDate) => setStartDate(newStartDate)}
+        <div className={styles.datePicker}>
+          {/* Date Pick Field */}
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <p>Filter by Date</p>
+            <div className={styles.dateContainer}>
+              <DatePicker
+                label="Start"
+                slotProps={{
+                  textField: { size: "small" },
+                  actionBar: { actions: ["today"] },
+                }}
+                value={startDate}
+                onChange={(newStartDate) => setStartDate(newStartDate)}
+              />
+              <DatePicker
+                label="End"
+                slotProps={{
+                  textField: { size: "small" },
+                  actionBar: { actions: ["today"] },
+                }}
+                value={endDate}
+                onChange={(newEndDate) => setEndDate(newEndDate)}
+              />
+            </div>
+          </LocalizationProvider>
+
+          {/* Date Pick Presets */}
+          <Stack direction="row" spacing={1}>
+            <Chip
+              label="This Month"
+              onClick={() => handlePreset("thisMonth")}
             />
-            <DatePicker
-              label="End"
-              slotProps={{
-                textField: { size: "small" },
-                actionBar: { actions: ["today"] },
-              }}
-              value={endDate}
-              onChange={(newEndDate) => setEndDate(newEndDate)}
+            <Chip
+              label="Last Month"
+              onClick={() => handlePreset("lastMonth")}
             />
-          </div>
-        </LocalizationProvider>
+            <Chip label="Last 30 Days" onClick={() => handlePreset("last30")} />
+          </Stack>
+        </div>
+
+        {/* Total Amt Synopsis */}
+        <div className={styles.totalAmount}>
+          <p>Estimated Charges (USD) excluding taxes</p>
+          <h2 style={{ fontSize: "28px", fontWeight: "500" }}>$ 11.37</h2>
+        </div>
       </div>
 
       {/* Billing Table */}
       <table className={styles.table}>
+        {/* Table Header */}
         <thead className={styles.tableHeader}>
           <tr>
             <th>Services</th>
             <th>Unit Charges in $</th>
             <th>Usage</th>
-            <th>Amount</th>
+            <th className={styles.filterCol} onClick={toggleSort}>
+              Amount ($)
+              {sortDir === "asc" && (
+                <BsSortAlphaDown className={styles.filterIcon} />
+              )}
+              {sortDir === "desc" && (
+                <BsSortAlphaUp className={styles.filterIcon} />
+              )}
+            </th>
           </tr>
         </thead>
-        {billingData.map((bill) => (
-          <tbody key={bill.id}>
-            <MdKeyboardArrowDown className={styles.icon} />
-            <tr className={styles.toggleRow}>
-              <td>{bill.service}</td>
+
+        {/* Table Data */}
+        {sortedData.map((group) => (
+          <tbody key={group.id}>
+            <MdKeyboardArrowDown
+              className={`${styles.icon} ${open[group.id] ? styles.open : styles.closed}`}
+              onClick={() => toggle(group.id)}
+              aria-expanded={!!open[group.id]}
+            />
+            <tr
+              className={styles.toggleRow}
+              onClick={() => toggle(group.id)}
+              aria-expanded={!!open[group.id]}
+            >
+              <td>{group.service}</td>
               <td></td>
               <td></td>
               <td
-                className={`${styles.amtCol} ${getAmountLevel(bill.totalAmount)}`}
+                className={`${styles.amountCol} ${getAmountLevel(group.totalAmount)}`}
               >
-                {bill.totalAmount}
+                {group.totalAmount}
               </td>
             </tr>
 
-            {bill.children.map((child) => (
-              <tr key={child.id}>
+            {group.children.map((child) => (
+              <tr
+                key={child.id}
+                className={
+                  open[group.id] ? styles.openGroup : styles.closedGroup
+                }
+              >
                 <td>{child.name}</td>
                 <td>{child.unitCharge}</td>
                 <td className={getUsageLevel(child.usage)}>{child.usage}</td>
                 <td
-                  className={`${styles.amtCol} ${getAmountLevel(child.amount)}`}
+                  className={`${styles.amountCol} ${getAmountLevel(child.amount)}`}
                 >
                   {child.amount}
                 </td>
