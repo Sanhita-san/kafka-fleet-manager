dea7739699e527b8c7adc05de63a6eee236cfcf7
Sanhita-san <gssanhita@gmail.com>
2025-08-14 16:36:56 +0530

    Edited Unknown Page

--- src/components/unknown/Unknown.jsx
+++ src/components/unknown/Unknown.jsx
@@ -1,5 +1,12 @@
+import imgUrl from "../../assets/images/Mobile development-cuate.png";
+import styles from "./Unknown.module.css";
+
 const Unknown = () => {
-  return <div>Unknown</div>;
+  return (
+    <div className={styles.container}>
+      <img src={imgUrl} alt="Page Not Built" className={styles.mainImg} />
+    </div>
+  );
 };
 
 export default Unknown;
