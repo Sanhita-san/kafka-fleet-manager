7b65b5fadcf2391a699aab4c1ccf5ea15449e716
Sanhita-san <gssanhita@gmail.com>
2025-08-14 16:16:32 +0530

    Updated Billing Page

--- src/components/billing/BillingTable.jsx
+++ src/components/billing/BillingTable.jsx
@@ -76,118 +76,135 @@ export default function BillingTable() {
     <div className={styles.billingData}>
       <h2>Analysis</h2>
 
-      {/* Date Picker */}
-      <div className={styles.headerInfo}>
-        <div className={styles.datePicker}>
-          {/* Date Pick Field */}
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <p>Filter by Date</p>
-            <div className={styles.dateContainer}>
-              <DatePicker
-                label="Start"
-                slotProps={{
-                  textField: { size: "small" },
-                  actionBar: { actions: ["today"] },
-                }}
-                value={startDate}
-                onChange={(newStartDate) => setStartDate(newStartDate)}
-              />
-              <DatePicker
-                label="End"
-                slotProps={{
-                  textField: { size: "small" },
-                  actionBar: { actions: ["today"] },
-                }}
-                value={endDate}
-                onChange={(newEndDate) => setEndDate(newEndDate)}
-              />
-            </div>
-          </LocalizationProvider>
-
-          {/* Date Pick Presets */}
-          <Stack direction="row" spacing={1}>
-            <Chip
-              label="This Month"
-              onClick={() => handlePreset("thisMonth")}
-            />
-            <Chip
-              label="Last Month"
-              onClick={() => handlePreset("lastMonth")}
-            />
-            <Chip label="Last 30 Days" onClick={() => handlePreset("last30")} />
-          </Stack>
+      <div className={styles.tabsInfo}>
+        {/* Tabs */}
+        <div className={styles.tabs}>
+          <span className={`${styles.tab} ${styles.active}`}>Analysis</span>
+          <span className={styles.tab}>Invoices</span>
         </div>
 
-        {/* Total Amt Synopsis */}
-        <div className={styles.totalAmount}>
-          <p>Estimated Charges (USD) excluding taxes</p>
-          <h2 style={{ fontSize: "28px", fontWeight: "500" }}>$ 11.37</h2>
+        {/* Tabs Data */}
+        <div className={styles.tabsData}>
+          <h2 className={styles.fleetName}>Acme Fleet</h2>
+          {/* Date Picker */}
+          <div className={styles.headerInfo}>
+            <div className={styles.datePicker}>
+              {/* Date Pick Field */}
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <p>Filter by Date</p>
+                <div className={styles.dateContainer}>
+                  <DatePicker
+                    label="Start"
+                    slotProps={{
+                      textField: { size: "small" },
+                      actionBar: { actions: ["today"] },
+                    }}
+                    value={startDate}
+                    onChange={(newStartDate) => setStartDate(newStartDate)}
+                  />
+                  <DatePicker
+                    label="End"
+                    slotProps={{
+                      textField: { size: "small" },
+                      actionBar: { actions: ["today"] },
+                    }}
+                    value={endDate}
+                    onChange={(newEndDate) => setEndDate(newEndDate)}
+                  />
+                </div>
+              </LocalizationProvider>
+
+              {/* Date Pick Presets */}
+              <Stack direction="row" spacing={1}>
+                <Chip
+                  label="This Month"
+                  onClick={() => handlePreset("thisMonth")}
+                />
+                <Chip
+                  label="Last Month"
+                  onClick={() => handlePreset("lastMonth")}
+                />
+                <Chip
+                  label="Last 30 Days"
+                  onClick={() => handlePreset("last30")}
+                />
+              </Stack>
+            </div>
+
+            {/* Total Amt Synopsis */}
+            <div className={styles.totalAmount}>
+              <p>Estimated Charges (USD) excluding taxes</p>
+              <h2 style={{ fontSize: "28px", fontWeight: "500" }}>$ 11.37</h2>
+            </div>
+          </div>
+
+          {/* Billing Table */}
+          <table className={styles.table}>
+            {/* Table Header */}
+            <thead className={styles.tableHeader}>
+              <tr>
+                <th>Services</th>
+                <th>Unit Charges in $</th>
+                <th>Usage</th>
+                <th className={styles.filterCol} onClick={toggleSort}>
+                  Amount ($)
+                  {sortDir === "asc" && (
+                    <BsSortAlphaDown className={styles.filterIcon} />
+                  )}
+                  {sortDir === "desc" && (
+                    <BsSortAlphaUp className={styles.filterIcon} />
+                  )}
+                </th>
+              </tr>
+            </thead>
+
+            {/* Table Data */}
+            {sortedData.map((group) => (
+              <tbody key={group.id}>
+                <MdKeyboardArrowDown
+                  className={`${styles.icon} ${open[group.id] ? styles.open : styles.closed}`}
+                  onClick={() => toggle(group.id)}
+                  aria-expanded={!!open[group.id]}
+                />
+                <tr
+                  className={styles.toggleRow}
+                  onClick={() => toggle(group.id)}
+                  aria-expanded={!!open[group.id]}
+                >
+                  <td>{group.service}</td>
+                  <td></td>
+                  <td></td>
+                  <td
+                    className={`${styles.amountCol} ${getAmountLevel(group.totalAmount)}`}
+                  >
+                    {group.totalAmount}
+                  </td>
+                </tr>
+
+                {group.children.map((child) => (
+                  <tr
+                    key={child.id}
+                    className={
+                      open[group.id] ? styles.openGroup : styles.closedGroup
+                    }
+                  >
+                    <td>{child.name}</td>
+                    <td>{child.unitCharge}</td>
+                    <td className={getUsageLevel(child.usage)}>
+                      {child.usage}
+                    </td>
+                    <td
+                      className={`${styles.amountCol} ${getAmountLevel(child.amount)}`}
+                    >
+                      {child.amount}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            ))}
+          </table>
         </div>
       </div>
-
-      {/* Billing Table */}
-      <table className={styles.table}>
-        {/* Table Header */}
-        <thead className={styles.tableHeader}>
-          <tr>
-            <th>Services</th>
-            <th>Unit Charges in $</th>
-            <th>Usage</th>
-            <th className={styles.filterCol} onClick={toggleSort}>
-              Amount ($)
-              {sortDir === "asc" && (
-                <BsSortAlphaDown className={styles.filterIcon} />
-              )}
-              {sortDir === "desc" && (
-                <BsSortAlphaUp className={styles.filterIcon} />
-              )}
-            </th>
-          </tr>
-        </thead>
-
-        {/* Table Data */}
-        {sortedData.map((group) => (
-          <tbody key={group.id}>
-            <MdKeyboardArrowDown
-              className={`${styles.icon} ${open[group.id] ? styles.open : styles.closed}`}
-              onClick={() => toggle(group.id)}
-              aria-expanded={!!open[group.id]}
-            />
-            <tr
-              className={styles.toggleRow}
-              onClick={() => toggle(group.id)}
-              aria-expanded={!!open[group.id]}
-            >
-              <td>{group.service}</td>
-              <td></td>
-              <td></td>
-              <td
-                className={`${styles.amountCol} ${getAmountLevel(group.totalAmount)}`}
-              >
-                {group.totalAmount}
-              </td>
-            </tr>
-
-            {group.children.map((child) => (
-              <tr
-                key={child.id}
-                className={
-                  open[group.id] ? styles.openGroup : styles.closedGroup
-                }
-              >
-                <td>{child.name}</td>
-                <td>{child.unitCharge}</td>
-                <td className={getUsageLevel(child.usage)}>{child.usage}</td>
-                <td
-                  className={`${styles.amountCol} ${getAmountLevel(child.amount)}`}
-                >
-                  {child.amount}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        ))}
-      </table>
     </div>
   );
 }
